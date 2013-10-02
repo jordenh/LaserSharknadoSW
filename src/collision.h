cec148218e9c835c3a72819b3afc2fe0a052007b
Gord Larson <gordlarson@gmail.com>
2013-10-01 18:35:06 -0700

    Add displacement functions, separated stubs into their own file

--- src/collision.h
+++ src/collision.h
@@ -1,31 +1,7 @@
 #ifndef COLLISION_H
 #define COLLISION_H
 
-#define RIGHT 1
-#define LEFT -1
-#define TRUE 1
-#define FALSE 0
-#define SHARK_LENGTH 37
-#define SHARK_HEIGHT 20
-#define PLAYER_LENGTH 20
-#define PLAYER_HEIGHT 20
-
-// STUB
-typedef struct {
-	int x;
-	int y;
-} Shark;
-
-typedef struct {
-	int x;
-	int y;
-} Player;
-
-typedef struct {
-	int x;
-	int y;
-	int direction;
-} Bullet;
+#include "stubs.h"
 
 int isBulletCollidingWithShark(Shark *shark, Bullet *bullet);
 int isBulletCollidingWithPlayer(Player *player, Bullet *bullet);
