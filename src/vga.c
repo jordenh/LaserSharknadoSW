5d08625fb70d2d6a969e164ed01d62f2aa0bc197
BlueMonday <stevenerenst@gmail.com>
2013-10-01 23:00:09 -0700

    renamed vga initialization function

--- src/vga.c
+++ src/vga.c
@@ -1,6 +1,6 @@
 #include "vga.h"
 
-void setupVga()
+void initVga()
 {
 	pixel_buffer = alt_up_pixel_buffer_dma_open_dev("/dev/pixel_buffer_dma");
 
@@ -26,7 +26,7 @@ void clearScreen()
 
 void drawLine(int x0, int y0, int x1, int y1, int color)
 {
-	/*if (y0 == y1)
+	if (y0 == y1)
 	{
 		alt_up_pixel_buffer_dma_draw_hline(pixel_buffer, x0, x1, y0, color, 0);
 	}
@@ -35,9 +35,9 @@ void drawLine(int x0, int y0, int x1, int y1, int color)
 		alt_up_pixel_buffer_dma_draw_hline(pixel_buffer, x0, y0, y1, color, 0);
 	}
 	else
-	{*/
+	{
 		alt_up_pixel_buffer_dma_draw_line(pixel_buffer, x0, y0, x1, y1, color, 0);
-	//}
+	}
 }
 
 void printLine(void)
