eb885b5010248778e52541b295b96a5acdcd7e08
sarahlynn <sarah.holdijk@gmail.com>
2013-10-01 21:45:53 -0700

    bullets... oops

---
+++ src/bullet.h
@@ -0,0 +1,25 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include "vga.h"
+
+#define NUM_BULLETS 128
+#define BULLET_LENGTH 3
+
+typedef enum {
+	PLAYERBULLET,
+	SHARKBULLET,
+	NOTACTIVE
+} bulletstatus;
+
+typedef struct {
+	int x;
+	int y;
+	bulletstatus status;
+} Bullet;
+
+void drawBullet(Bullet *bullet);
+void eraseBullet(Bullet *bullet);
+void moveRight(Bullet *bullet);
+
+#endif
