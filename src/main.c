714fab42bcabb85529e6a8093af2546176684a4b
Jorden Hetherington <jorden.hetherington@gmail.com>
2013-10-03 21:00:29 -0700

    merged double buffering code, and all cleaning by sarah and steven

--- src/main.c
+++ src/main.c
@@ -106,78 +106,7 @@ int main() {
 		if (hasHardwareTimerExpired() == 1) {
 			startHardwareTimer();
 			count++;
-/*<<<<<<< HEAD
-			if (count % 30 == 0) {
-				printf("%i: Timer has expired\n", count / 30);
-			}
 
-			SWInput = IORD_8DIRECT(switches, 0);
-			keyInput = IORD_8DIRECT(keys, 0);
-			atariButtons = (IORD_8DIRECT(atariInput, 0) & 0x0F);
-			atariFire = atariButtons & 0x08;
-			atariUp = atariButtons & 0x02;
-			atariDown = atariButtons & 0x04;
-			IOWR_16DIRECT(leds, 0, atariButtons);
-
-			if ((atariFire == 0x00) && debounce == 0) {
-				debounce = 1;
-			} else if ((atariFire != 0x00) && debounce == 1){
-				debounce = 0;
-
-				int index = 0;
-				while (index < NUM_BULLETS) {
-					if (bulletArray[index].status == NOTACTIVE) {
-						bulletArray[index].x = player.x + PLAYER_WIDTH + 1;
-						bulletArray[index].y = player.y + 0.5 * PLAYER_HEIGHT;
-						bulletArray[index].status = PLAYERBULLET;
-						drawBullet(&bulletArray[index]);
-						break;
-					}
-					index++;
-				}
-
-				//playLaser();
-				//playTheme();
-			}
-
-			if (count % 2 == 0) {
-				i = 0;
-				for (i = 0; i < NUM_BULLETS; i++) {
-					if (bulletArray[i].status != NOTACTIVE) {
-						moveRight(&bulletArray[i]);
-					}
-				}
-			}
-
-			if (atariUp != 0x00) {
-				//printf("Key 0");
-				moveUp(&player);
-			}
-			if (atariDown != 0x00) {
-				//printf("Key 1");
-				moveDown(&player);
-			}
-			if ((SWInput & 0x80) != 0) {
-				if(scoresShown == 0){
-					for(i = 0; i < NUMSCORES; i++) {
-						scoreInitials[0] = gameScores.highScoreBoardInits[i][0];
-						scoreInitials[1] = gameScores.highScoreBoardInits[i][1];
-						scoreInitials[2] = gameScores.highScoreBoardInits[i][2];
-
-						//for(j = 0; j < )
-
-						alt_up_char_buffer_string(char_buffer,scoreInitials , 40, 30 + i);
-					}
-				}
-				scoresShown = 1;
-			} else {
-				if(scoresShown == 1){
-					alt_up_char_buffer_clear(char_buffer);
-				}
-				scoresShown = 0;
-			}
-
-======= */
 			keyInput = IORD_8DIRECT(KEYS_BASE, 0);
 			SWInput = IORD_8DIRECT(switches, 0);
 			atariButtons = (IORD_8DIRECT(atariInput, 0) & 0x0F);
@@ -228,6 +157,16 @@ int main() {
 				}
 				scoresShown = 0;
 			}
+			if((keyInput & 0x02) != 0x00){
+				playPlayerDeath();
+			}
+			if((keyInput & 0x04) != 0x00){
+				playTheme();
+			}
+			if((keyInput & 0x08) != 0x00){
+				playSharkDeath();
+			}
+
 
 			drawAllBullets();
 
@@ -237,7 +176,6 @@ int main() {
 			eraseShark(shark1);
 			eraseAllBullets();
 			erasePlayer();
-//>>>>>>> cc52c7640605ea4841072893b27068e7149bfb29
 		}
 	}
 
