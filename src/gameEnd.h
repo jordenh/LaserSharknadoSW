7e3baf108f6a1d88d7e5519b17c914995a08603a
Jorden Hetherington <jorden.hetherington@gmail.com>
2013-10-10 17:50:14 -0700

    removed uneccesary includes

--- src/gameEnd.h
+++ src/gameEnd.h
@@ -5,19 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "audio.h"
-#include "timer.h"
 #include "sd_card.h"
 #include "vga.h"
-#include "bmp.h"
 #include "shark.h"
-#include "displacement.h"
-#include "collision.h"
-#include "io.h"
-#include "system.h"
-#include "altera_nios2_qsys_irq.h"
-#include "sys/alt_irq.h"
 #include "score.h"
-#include "input.h"
 #include "splash.h"
 
 
